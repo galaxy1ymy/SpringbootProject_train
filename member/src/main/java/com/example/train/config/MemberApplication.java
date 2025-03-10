302ab2791acc8a3d5e177b90ceb9851ac2134386
galaxy1ymy <y251325031228@163.com>
2025-03-10 14:27:58 +0800

    增加http测试文件，方便直接在IDEA中测试

--- member/src/main/java/com/example/train/config/MemberApplication.java
+++ member/src/main/java/com/example/train/config/MemberApplication.java
@@ -14,7 +14,7 @@ public class MemberApplication {
         SpringApplication app=new SpringApplication(MemberApplication.class);
         Environment env=app.run(args).getEnvironment();
         LOG.info("启动成功！");
-        LOG.info("测试地址：\thttp://127.0.0.1:{}/hello",env.getProperty("server.port"));//启动端口
+        LOG.info("测试地址：\thttp://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));//启动端口
     }
 
 
