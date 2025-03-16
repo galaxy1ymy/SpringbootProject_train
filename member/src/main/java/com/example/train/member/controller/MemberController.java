8ab382b5635e45a4ad00803a616ba1b0b362044b
galaxy1ymy <y251325031228@163.com>
2025-03-16 17:04:45 +0800

    封装请求参数和返回结果

--- member/src/main/java/com/example/train/member/controller/MemberController.java
+++ member/src/main/java/com/example/train/member/controller/MemberController.java
@@ -1,4 +1,6 @@
 package com.example.train.member.controller;
+import com.example.train.common.resp.CommonResp;
+import com.example.train.member.reg.MemberRegisterReq;
 import com.example.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,19 @@ public class MemberController {
     private MemberService memberService;
 
     @GetMapping("/count")
-    public Integer count() {
-        return memberService.count();
+    public CommonResp<Integer> count() {
+        int count = memberService.count();
+        CommonResp<Integer> commonResp= new CommonResp<>();
+        commonResp.setContent(count);
+        return commonResp;
     }
 
     @PostMapping("/register")
-    public long register(String mobile) {
-        return memberService.register(mobile);
+    public CommonResp<Long> register(MemberRegisterReq req) {
+        long register = memberService.register(req);
+//        CommonResp<Long> commonResp= new CommonResp<>();
+//        commonResp.setContent(register);
+//        return commonResp;
+        return new CommonResp<>(register);
     }
 }
---
+++ member/src/main/java/com/example/train/member/reg/MemberRegisterReq.java
@@ -0,0 +1,20 @@
+package com.example.train.member.reg;
+
+public class MemberRegisterReq {
+    public String mobile;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberRegisterReq{" +
+                "mobile='" + mobile + '\'' +
+                '}';
+    }
+}
