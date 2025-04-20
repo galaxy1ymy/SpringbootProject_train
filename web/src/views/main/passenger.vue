61886a9f4b8b521e410df903e36a9a1572cd54a2
galaxy1ymy <y251325031228@163.com>
2025-04-20 17:55:05 +0800

    前端枚举展示的解决方案,解决前端乘车人类型问题

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -22,6 +22,13 @@
             <a @click="onEdit(record)">编辑</a>
           </a-space>
         </template>
+        <template v-else-if="column.dataIndex === 'type'">
+          <span v-for="item in PASSENGER_TYPE_ARRAY" :key="item.key">
+            <span v-if="item.key===record.type">
+              {{item.value}}
+            </span>
+          </span>
+        </template>
       </template>
     </a-table>
     <a-modal v-model:open="visible" title="乘车人" @ok="handleOk"
@@ -35,9 +42,7 @@
         </a-form-item>
         <a-form-item label="类型">
           <a-select v-model:value="passenger.type">
-            <a-select-option value="1">成人</a-select-option>
-            <a-select-option value="2">儿童</a-select-option>
-            <a-select-option value="3">学生</a-select-option>
+            <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.key" :value="item.key">{{item.value}}</a-select-option>
           </a-select>
         </a-form-item>
       </a-form>
@@ -53,6 +58,7 @@ export default defineComponent({
   name: "passenger",
   setup() {
     const visible = ref(false);
+    const PASSENGER_TYPE_ARRAY=window.PASSENGER_TYPE_ARRAY;
     let passenger = ref({
       id: undefined,
       memberId: undefined,
@@ -179,6 +185,7 @@ export default defineComponent({
 
     return{
       visible,
+      PASSENGER_TYPE_ARRAY,
       onAdd,
       onEdit,
       handleOk,
