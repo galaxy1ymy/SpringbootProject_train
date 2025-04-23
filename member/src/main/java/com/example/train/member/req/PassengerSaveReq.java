3c9f1f2ed66ec01abf078db72466bdef5fd9e965
galaxy1ymy <y251325031228@163.com>
2025-04-23 21:18:32 +0800

    生成saveReq实体类

--- member/src/main/java/com/example/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/example/train/member/req/PassengerSaveReq.java
@@ -1,86 +1,109 @@
 package com.example.train.member.req;
 
+import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.util.Date;
 
 public class PassengerSaveReq {
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private Long id;
 
-    private Long memberId;
+    /**
+     * id
+     */
+     @JsonFormat(shape = JsonFormat.Shape.STRING)
+     private Long id;
 
-    @NotBlank(message = "[姓名]不能为空！")
-    private String name;
+    /**
+     * 会员id
+     */
+     private Long memberId;
 
-    @NotBlank(message = "[身份证号]不能为空！")
-    private String idCard;
+    /**
+     * 姓名
+     */
+     @NotBlank(message = "[姓名]不能为空！")
+     private String name;
 
-    @NotBlank(message = "[旅客类型]不能为空！")
-    private String type;
+    /**
+     * 身份证
+     */
+     @NotBlank(message = "[身份证]不能为空！")
+     private String idCard;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date createTime;
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
+     @NotBlank(message = "[旅客类型]不能为空！")
+     private String type;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date updateTime;
+    /**
+     * 新增时间
+     */
+     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+     private Date createTime;
 
-    public Long getId() {
+    /**
+     * 修改时间
+     */
+     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+     private Date updateTime;
+
+    public Long getid(){
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(Long id){
+        this.id=id;
     }
 
-    public Long getMemberId() {
+    public Long getmemberId(){
         return memberId;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+    public void setMemberId(Long memberId){
+        this.memberId=memberId;
     }
 
-    public String getName() {
+    public String getname(){
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name){
+        this.name=name;
     }
 
-    public String getIdCard() {
+    public String getidCard(){
         return idCard;
     }
 
-    public void setIdCard(String idCard) {
-        this.idCard = idCard;
+    public void setIdCard(String idCard){
+        this.idCard=idCard;
     }
 
-    public String getType() {
+    public String gettype(){
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setType(String type){
+        this.type=type;
     }
 
-    public Date getCreateTime() {
+    public Date getcreateTime(){
         return createTime;
     }
 
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
+    public void setCreateTime(Date createTime){
+        this.createTime=createTime;
     }
 
-    public Date getUpdateTime() {
+    public Date getupdateTime(){
         return updateTime;
     }
 
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
+    public void setUpdateTime(Date updateTime){
+        this.updateTime=updateTime;
     }
 
     @Override
@@ -89,13 +112,13 @@ public class PassengerSaveReq {
         sb.append(getClass().getSimpleName());
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", memberId=").append(memberId);
-        sb.append(", name=").append(name);
-        sb.append(", idCard=").append(idCard);
-        sb.append(", type=").append(type);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", updateTime=").append(updateTime);
+        sb.append("id=").append(id);
+        sb.append("memberId=").append(memberId);
+        sb.append("name=").append(name);
+        sb.append("idCard=").append(idCard);
+        sb.append("type=").append(type);
+        sb.append("createTime=").append(createTime);
+        sb.append("updateTime=").append(updateTime);
         sb.append("]");
         return sb.toString();
     }
