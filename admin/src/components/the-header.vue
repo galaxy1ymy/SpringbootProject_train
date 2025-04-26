5a20a0a0c6c07e2904c6ac297440189f8e1f92da
galaxy1ymy <y251325031228@163.com>
2025-04-26 17:18:54 +0800

    创建admin模块，从web模块复制

---
+++ admin/src/components/the-header.vue
@@ -0,0 +1,59 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo"/>
+    <div style="float: right;color: white">
+      您好,{{member.mobile}}！&nbsp;&nbsp;
+      <router-link to="/login" style="color: white">退出登录</router-link>
+    </div>
+    <a-menu theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }" :selectedKeys="selectedKeys">
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome" style="color: inherit;">
+          <CoffeeOutlined/>&nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger" style="color: inherit;">
+          <UserOutlined/>&nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import {defineComponent, ref, watch} from 'vue';
+import store from "@/store";
+import router from "@/router";
+
+export default defineComponent({
+  name:"the-header-view",
+  setup() {
+
+    const selectedKeys = ref([]);
+    let member=store.state.member
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch',newValue);
+      selectedKeys.value=[];
+      selectedKeys.value.push(newValue);
+    }, { immediate: true });
+
+
+    return{
+      selectedKeys,
+      member
+
+    };
+  }
+})
+</script>
+
+
+<style scoped>
+.logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+</style>
---
+++ admin/src/views/main.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-layout>
+    <the-header-view></the-header-view>
+    <a-layout>
+      <the-sider-view></the-sider-view>
+      <a-layout >
+        <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+          <router-view></router-view>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import {defineComponent} from 'vue';
+import TheHeaderView from "@/components/the-header.vue";
+import TheSiderView from "@/components/the-sider.vue";
+
+export default defineComponent({
+  components:{
+    TheSiderView,
+    TheHeaderView
+  },
+  setup() {
+    return {
+    };
+  },
+});
+
+</script>
+<style scoped>
+
+</style>
