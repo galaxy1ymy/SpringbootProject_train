134ba1dfe55c34b85b58c96db59cc6e1879843cb
galaxy1ymy <y251325031228@163.com>
2025-05-15 13:00:32 +0800

    禁止任务并发执行

--- batch/src/main/java/com/example/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/example/train/batch/job/SpringBootTestJob.java
@@ -1,13 +1,16 @@
+/*
 package com.example.train.batch.job;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+*/
 /**
  * 适合单体应用，不适合集群
  * 没法实时更改定时任务状态和策略
- */
+ *//*
+
 @Component
 @EnableScheduling
 public class SpringBootTestJob {
@@ -20,3 +23,4 @@ public class SpringBootTestJob {
         System.out.println("SpringBootTestJob Test");
     }
 }
+*/
--- batch/src/main/java/com/example/train/batch/job/TestJob.java
+++ batch/src/main/java/com/example/train/batch/job/TestJob.java
@@ -1,13 +1,22 @@
 package com.example.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+//禁用并发执行
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob Test");
+        System.out.println("TestJob Test开始");
+        try{
+            Thread.sleep(3000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("TestJob Test结束");
     }
 }
