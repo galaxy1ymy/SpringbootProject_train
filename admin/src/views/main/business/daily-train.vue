88bfb04fc6e7ea94b4bb2644f33c51d83118495a
galaxy1ymy <y251325031228@163.com>
2025-05-22 21:29:49 +0800

    手动生成车次功能增加loading,防止重复生成

--- admin/src/views/main/business/daily-train.vue
+++ admin/src/views/main/business/daily-train.vue
@@ -72,7 +72,9 @@
         </a-form-item>
       </a-form>
     </a-modal>
-    <a-modal  v-model:open="genDailyVisible" title="生成车次" @ok="handleGenDailyOk"
+    <a-modal  v-model:open="genDailyVisible" title="生成车次"
+              :confirm-loading="genDailyLoading"
+              @ok="handleGenDailyOk"
               ok-text="确认" cancel-text="取消">
       <a-form :model="genDaily" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }">
         <a-form-item label="日期">
@@ -126,6 +128,7 @@ export default defineComponent({
       date:null
     });
     const genDailyVisible = ref(false);
+    const genDailyLoading = ref(false);
     const columns = [
        {
          title: '日期',
@@ -271,7 +274,9 @@ export default defineComponent({
 
     const handleGenDailyOk = () => {
       let date=dayjs(genDaily.value.date).format('YYYY-MM-DD');
+      genDailyLoading.value=true;
       axios.get('/business/admin/daily-train/gen-daily/'+date).then(response => {
+        genDailyLoading.value  = false;
         let data = response.data;
         if (data.success) {
           notification.success({ description: '生成成功' });
@@ -316,7 +321,8 @@ export default defineComponent({
       onClickGenDaily,
       genDailyVisible,
       genDaily,
-      handleGenDailyOk
+      handleGenDailyOk,
+      genDailyLoading
     }
   }
 })
