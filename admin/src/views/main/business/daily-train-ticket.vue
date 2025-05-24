e8d25713b69735ba3597acaedefd865561e8850f
galaxy1ymy <y251325031228@163.com>
2025-05-24 17:16:12 +0800

    优化余票页面

--- admin/src/views/main/business/daily-train-ticket.vue
+++ admin/src/views/main/business/daily-train-ticket.vue
@@ -16,6 +16,59 @@
       <template #bodyCell="{column,record}">
         <template v-if="column.dataIndex === 'operation'">
         </template>
+        <template v-else-if="column.dataIndex ==='station'">
+          {{record.start}}<br/>
+          {{record.end}}
+        </template>
+        <template v-else-if="column.dataIndex ==='time'">
+          {{record.startTime}}<br/>
+          {{record.endTime}}
+        </template>
+        <template v-else-if="column.dataIndex ==='duration'">
+          {{calDuration(record.startTime,record.endTime)}}<br/>
+          <div v-if="record.startTime.replaceAll(':','') >= record.endTime.replaceAll(':','')">
+            次日到达
+          </div>
+          <div v-else>
+            当日到达
+          </div>
+        </template>
+        <template v-else-if="column.dataIndex ==='ydz'">
+          <div v-if="record.ydz >=0">
+            {{record.ydz}}<br/>
+            {{record.ydzPrice}}￥
+          </div>
+          <div v-else>
+            --
+          </div>
+        </template>
+        <template v-else-if="column.dataIndex ==='edz'">
+          <div v-if="record.edz >=0">
+            {{record.edz}}<br/>
+            {{record.edzPrice}}￥
+          </div>
+          <div v-else>
+            --
+          </div>
+        </template>
+        <template v-else-if="column.dataIndex ==='rw'">
+          <div v-if="record.rw >=0">
+            {{record.rw }}<br/>
+            {{record.rwPrice}}￥
+          </div>
+          <div v-else>
+            --
+          </div>
+        </template>
+        <template v-else-if="column.dataIndex ==='yw'">
+          <div v-if="record.yw >=0">
+            {{record.yw}}<br/>
+            {{record.ywPrice}}￥
+          </div>
+          <div v-else>
+            --
+          </div>
+        </template>
       </template>
     </a-table>
 </template>
@@ -26,6 +79,7 @@ import {notification} from "ant-design-vue";
 import axios from "axios";
 import TrainSelectView from "@/components/train-select.vue";
 import StationSelectView from "@/components/station-select.vue";
+import dayjs from "dayjs";
 
 export default defineComponent({
   name: "daily-train-ticket-view",
@@ -81,7 +135,19 @@ export default defineComponent({
          dataIndex: 'trainCode',
          key: 'trainCode',
        },
-       {
+      {
+        title:'车站',
+        dataIndex: 'station'
+      },
+      {
+        title:'时间',
+        dataIndex: 'time'
+      },
+      {
+        title:'历时',
+        dataIndex: 'duration'
+      },
+      /* {
          title: '出发站',
          dataIndex: 'start',
          key: 'start',
@@ -120,47 +186,47 @@ export default defineComponent({
          title: '到达站序',
          dataIndex: 'endIndex',
          key: 'endIndex',
-       },
+       },*/
        {
-         title: '一等座余票',
+         title: '一等座',
          dataIndex: 'ydz',
          key: 'ydz',
        },
-       {
+       /*{
          title: '一等座票价',
          dataIndex: 'ydzPrice',
          key: 'ydzPrice',
-       },
+       },*/
        {
-         title: '二等座余票',
+         title: '二等座',
          dataIndex: 'edz',
          key: 'edz',
        },
-       {
+       /*{
          title: '二等座票价',
          dataIndex: 'edzPrice',
          key: 'edzPrice',
-       },
+       },*/
        {
-         title: '软卧余票',
+         title: '软卧',
          dataIndex: 'rw',
          key: 'rw',
        },
-       {
+      /* {
          title: '软卧票价',
          dataIndex: 'rwPrice',
          key: 'rwPrice',
-       },
+       },*/
        {
-         title: '硬卧余票',
+         title: '硬卧',
          dataIndex: 'yw',
          key: 'yw',
        },
-       {
+      /* {
          title: '硬卧票价',
          dataIndex: 'ywPrice',
          key: 'ywPrice',
-       },
+       },*/
       ];
 
 
@@ -203,6 +269,11 @@ export default defineComponent({
           })
       }
 
+      const calDuration=(startTime,endTime)=>{
+      let diff=dayjs(endTime,'HH:mm:ss').diff(dayjs(startTime,'HH:mm:ss'),'seconds');
+      return dayjs('00:00:00','HH:mm:ss').second(diff).format('HH:mm:ss');
+      }
+
     //界面渲染好后执行
     onMounted(()=>{
       handleQuery({
@@ -222,7 +293,8 @@ export default defineComponent({
       handleTableChange,
       handleQuery,
       loading,
-      params
+      params,
+      calDuration
     }
   }
 })
