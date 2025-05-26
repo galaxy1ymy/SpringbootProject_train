d513e474fb9a39c653136fa752800b5f0219e8fe
galaxy1ymy <y251325031228@163.com>
2025-05-26 18:00:16 +0800

    订单页面显示座位信息

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -7,6 +7,14 @@
     <span class="order-train-main">————</span>&nbsp;
     <span class="order-train-main">{{dailyTrainTicket.end}}</span>站
     <span class="order-train-main">({{dailyTrainTicket.endTime}})</span>&nbsp;
+
+    <div class="order-train-ticket">
+      <span v-for="item in seatTypes" :key="item.type">
+        <span>{{item.desc}}</span>:
+        <span class="order-train-ticket-main">{{item.price}}￥</span>&nbsp;
+        <span class="order-train-ticket-main">{{item.count}}</span>&nbsp;张票&nbsp;&nbsp;
+      </span>
+    </div>
   </div>
 </template>
 
@@ -18,9 +26,25 @@ export default defineComponent({
   setup(){
     const dailyTrainTicket=SessionStorage.get(SESSION_ORDER) || {};
     console.log("下单的车次信息",dailyTrainTicket);
+    const SEAT_TYPE=window.SEAT_TYPE;
+    const seatTypes=[];
+    for(let KEY in SEAT_TYPE){
+      let key=KEY.toLowerCase();
+      if(dailyTrainTicket[key]>=0){
+        seatTypes.push({
+          type:KEY,
+          code:SEAT_TYPE[KEY]["code"],
+          desc:SEAT_TYPE[KEY]["desc"],
+          count:dailyTrainTicket[key],
+          price:dailyTrainTicket[key+'Price'],
+        })
+      }
+    }
+    console.log("本次提供的座位：",seatTypes);
 
     return{
-      dailyTrainTicket
+      dailyTrainTicket,
+      seatTypes
     }
   }
 })
@@ -31,5 +55,15 @@ export default defineComponent({
   font-size: 18px;
   font-weight: bold;
 }
+.order-train-ticket {
+  font-size: 16px;
+  margin-top: 30px;
+}
+.order-train-ticket-main {
+  font-size: 18px;
+  color: red;
+}
+
+
 
 </style>
