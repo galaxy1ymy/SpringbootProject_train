a0749126107bc967427402c1d75f244b2f03e85f
galaxy1ymy <y251325031228@163.com>
2025-06-19 19:04:16 +0800

    集成Nacos配置中心，实现动态刷新的功能

--- member/src/main/java/com/example/train/member/controller/TestController.java
+++ member/src/main/java/com/example/train/member/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.example.train.member.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+//spring cloud注解，用于动态更新nacos配置
+@RefreshScope
 public class TestController {
 
     @Value("${test.nacos}")
