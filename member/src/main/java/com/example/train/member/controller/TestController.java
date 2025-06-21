27010263f1e2c4b9b95a84639769c4f1e4b2844c
galaxy1ymy <y251325031228@163.com>
2025-06-21 13:15:25 +0800

    配置gateway+Nacos支持应用名路由转发

--- member/src/main/java/com/example/train/member/controller/TestController.java
+++ member/src/main/java/com/example/train/member/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.example.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,12 @@ public class TestController {
     @Value("${test.nacos}")
     private String testNacos;
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/hello")
     public String hello() {
-        return String.format("hello %s", testNacos);
+        String port=environment.getProperty("local.server.port");
+        return String.format("hello %s! 端口: %s", testNacos,port);
     }
 }
