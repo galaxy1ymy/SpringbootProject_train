6942b477fc3113dbc654d1c71b1af0d7dccf396c
galaxy1ymy <y251325031228@163.com>
2025-07-15 17:57:16 +0800

    Sentinel+Feign熔段

--- business/src/main/java/com/example/train/business/controller/TestController.java
+++ business/src/main/java/com/example/train/business/controller/TestController.java
@@ -1,14 +1,27 @@
 package com.example.train.business.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.example.train.business.service.TestService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Resource
+    private TestService testService;
+
     @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello() throws InterruptedException{
+        testService.hello2();
+        return "hello world!Business!";
+    }
+
+    @SentinelResource("hello1")
+    @GetMapping("/hello1")
+    public String hello1() throws InterruptedException{
+        testService.hello2();
         return "hello world!Business!";
     }
 }
