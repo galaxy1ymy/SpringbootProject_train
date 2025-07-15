1d41b4a79b8c2d138145721bbda42f3da4eff67e
galaxy1ymy <y251325031228@163.com>
2025-07-15 17:02:31 +0800

    Sentinel限流不同的流控效果 - Warm Up&排队等待

--- business/src/main/java/com/example/train/business/controller/TestController.java
+++ business/src/main/java/com/example/train/business/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.example.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @SentinelResource("hello")
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws InterruptedException{
         return "hello world!Business!";
     }
 }
